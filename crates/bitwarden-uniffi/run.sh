4a6968eae9be9d558ee303d092f70578c38ce221
Hinton <hinton@users.noreply.github.com>
2023-05-18 18:43:49 +0200

    Get android working

--- crates/bitwarden-uniffi/run.sh
+++ crates/bitwarden-uniffi/run.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+# Swift
+
 cargo run --bin uniffi-bindgen generate src/sdk.udl --language swift
 cargo build --package bitwarden-uniffi --target aarch64-apple-ios-sim --release
 
@@ -7,7 +9,16 @@ mkdir Headers
 cp ./src/bitwardenFFI.h ./Headers/
 cp ./src/bitwardenFFI.modulemap ./Headers/module.modulemap
 
-
 xcodebuild -create-xcframework -library ../../target/aarch64-apple-ios-sim/release/libbitwarden_uniffi.a -headers ./Headers -output BitwardenSdk.xcframework
 
 cp src/bitwarden.swift platforms/apple/Hello/Sources/Hello
+
+# Kotlin
+
+rm -r bindings
+
+mkdir bindings
+mkdir bindings/swift
+mkdir bindings/kotlin
+
+cargo run --bin uniffi-bindgen generate src/sdk.udl --language kotlin --out-dir bindings/kotlin
