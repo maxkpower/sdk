6a170a07727b21e44f2de5b63ec2c547b29aaa38
Hinton <hinton@users.noreply.github.com>
2023-05-19 19:06:12 +0200

    Cleanup run file

--- crates/bitwarden-uniffi/run.sh
+++ crates/bitwarden-uniffi/run.sh
@@ -1,24 +1,31 @@
 #!/bin/env bash
 
-# Swift
-
-cargo run --bin uniffi-bindgen generate src/sdk.udl --language swift
-cargo build --package bitwarden-uniffi --target aarch64-apple-ios-sim --release
-
-mkdir Headers
-cp ./src/bitwardenFFI.h ./Headers/
-cp ./src/bitwardenFFI.modulemap ./Headers/module.modulemap
-
-xcodebuild -create-xcframework -library ../../target/aarch64-apple-ios-sim/release/libbitwarden_uniffi.a -headers ./Headers -output BitwardenSdk.xcframework
-
-cp src/bitwarden.swift platforms/apple/Hello/Sources/Hello
-
-# Kotlin
-
 rm -r bindings
 
 mkdir bindings
 mkdir bindings/swift
 mkdir bindings/kotlin
 
+rm -r dist
+mkdir dist/ios
+
+# Swift
+
+cargo run --bin uniffi-bindgen generate src/sdk.udl --language swift  --out-dir bindings/swift
+cargo build --package bitwarden-uniffi --target aarch64-apple-ios-sim --release
+
+mkdir bindings/swift/Headers
+
+mv ./bindings/swift/bitwardenFFI.h ./bindings/swift/Headers/
+mv ./bindings/swift/bitwardenFFI.modulemap ./bindings/swift/Headers/module.modulemap
+
+xcodebuild -create-xcframework -library ../../target/aarch64-apple-ios-sim/release/libbitwarden_uniffi.a -headers ./bindings/swift/Headers -output ./bindings/swift/BitwardenSdk.xcframework
+
+cp src/bitwarden.swift platforms/apple/Hello/Sources/Hello
+
+# Kotlin
+
 cargo run --bin uniffi-bindgen generate src/sdk.udl --language kotlin --out-dir bindings/kotlin
+cargo ndk -t arm64-v8a -o ./bindings/kotlin/jniLibs build
+
+# Copy files to android project
